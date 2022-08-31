c44584b640178137e98b71f88ed54b37217815af
arijit <arijitmaiti@technoexponent.com>
2022-08-31 17:38:22 +0530

    staff

--- server/payload-test/payload.config.js
+++ server/payload-test/payload.config.js
@@ -1,16 +1,8 @@
 import { buildConfig } from "payload/config";
-import path from "path";
-import dotenv from "dotenv";
 import Users from "./collections/Users";
 
-dotenv.config({
-  path: path.resolve(__dirname, "./.env"),
-});
-
-const PORT = process.env.PORT || 4000;
-
 export default buildConfig({
-  serverURL: `${process.env.SERVER_URL}:${PORT}`,
+  serverURL: "http://localhost:4000",
   admin: {
     user: Users.slug,
   },
